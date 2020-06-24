04606c9442978958fea9bdf7de83a910e4e6b77e
Khaled Elbalal <KhaledElbalal@gmail.com>
2020-06-24 18:57:35 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const { Nothing, isNothing } = require('nothing-mock');
 
 const win = (typeof window !== 'undefined') ? window : Nothing;
 const doc = (typeof document !== 'undefined') ? document : Nothing;
-
+const nav = (typeof navigator !== 'undefined') ? document : Nothing;
 module.exports.window = win;
 module.exports.document = doc;
+module.exports.navigator = nav;
 module.exports.exists = variable => !isNothing(variable)
